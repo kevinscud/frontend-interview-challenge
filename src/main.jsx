06532db15f256e856ecc51c36f5250d34254df29
Kevin Luse <kevinscud@gmail.com>
2024-05-03 00:00:26 +0300

    Refactor main

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import { RouterProvider, createRootRoute, createRoute } from '@tanstack/react-router'
-import { Router, RouterProvider } from "@tanstack/react-router";
-
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import router from './router.jsx';
-
 import './index.css'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            {/* <App /> */}
             <RouterProvider router={router} />
         </QueryClientProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
 )
