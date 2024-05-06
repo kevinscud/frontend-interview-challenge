fdfcbb25b8497f25c95f1615f89f5f03f72617cd
Kevin Luse <kevinscud@gmail.com>
2024-05-06 14:18:48 +0300

    Update styles and layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,11 @@ import { Link } from "@tanstack/react-router";
 const Header = () => {
     return (
         <header className='wrapper'>
-            {/* <div className="logo">
-
-            </div> */}
             <nav>
+                <div className="logo">
+                    <span className="icon material-symbols-rounded">task</span>
+                    <span className="text">Task App</span>
+                </div>
                 <Link icon='home' to="/" className="">Home</Link>
                 <Link icon='checklist' to="/tasks" activeOptions={{ exact: true }} className="">View Tasks</Link>
                 <Link icon='add' to="/create" className="">Create Task</Link>
